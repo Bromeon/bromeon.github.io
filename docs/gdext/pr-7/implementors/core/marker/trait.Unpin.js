08b1f014bf1276096e2fc70ee88ff23431733ff6
Godot-Rust Automation <GodotRust@users.noreply.github.com>
2023-05-07 09:31:46 +0000

    Update gdext PR #7 (`62be3f8`)
            Pull request: https://www.github.com/godot-rust/gdext/pull/7
            Commit:       https://www.github.com/godot-rust/gdext/pull/7/commits/62be3f8344ccf80bb4c9333daf5ff5ff9d94309a
            Triggered at: 2023-05-07T09:29:09Z

---
+++ docs/gdext/pr-7/implementors/core/marker/trait.Unpin.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"godot":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"godot/newsym/struct.Test.html\" title=\"struct godot::newsym::Test\">Test</a>",1,["godot::newsym::Test"]]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- website-docs-md/gdext-7.md
+++ website-docs-md/gdext-7.md
@@ -8,8 +8,8 @@ docs_rel_url = "gdext/pr-7/godot"
 pr_url = "https://github.com/godot-rust/gdext/pull/7"
 pr_author = "GodotRust"
 sort_key = 2023-05-07
-date_time = 2023-05-07T09:26:32Z
-commit_sha = "89f46e01a02df4babb0a7863fac937cf36caaaf4"
+date_time = 2023-05-07T09:29:09Z
+commit_sha = "62be3f8344ccf80bb4c9333daf5ff5ff9d94309a"
 +++
 
 Update ReadMe.md
